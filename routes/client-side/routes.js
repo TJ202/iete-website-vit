3a6da65cbf5ff77660d124f3d86c5e6967197338
TJ202 <72973488+TJ202@users.noreply.github.com>
2022-05-23 22:09:49 +0530

    client

--- routes/client-side/routes.js
+++ routes/client-side/routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+// Render static files
+router.use(express.static(__dirname + "public"));
+
 router.get("/", (req, res) => {
-    res.send("Home page of IETE website");
+    res.render("../views/client-side/pages/index.ejs");
+});
+
+router.get("/event", (req, res) => {
+    res.send("See the event");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
+
 const PORT = process.env.PORT || 5000;
 
 app.use("/", require("./routes/client-side/routes.js"));
+app.use("/public", express.static("public"));
+
+// Set the view engine to ejs
+app.set("view engine", ejs);
 
 app.listen(PORT, () =>
     console.log(`Server started on http://localhost:${PORT}`)
