296e79b3d7d02b9a1143b9e7617fa41fceb31233
TJ202 <72973488+TJ202@users.noreply.github.com>
2022-07-03 00:28:02 +0530

    final draft 2

--- controllers/coordinatorController.js
+++ controllers/coordinatorController.js
@@ -4,41 +4,38 @@ const { promisify } = require("util");
 const unlinkAsync = promisify(fs.unlink);
 const path = require("path");
 
-exports.faculty = async function (req, res) {
-  const fac = await FacCoordinator.find({});
-  res.render("../views/admin-side/pages/fac.ejs", { fac });
+exports.faculty = async function(req, res) {
+    const fac = await FacCoordinator.find({});
+    res.render("../views/admin-side/pages/fac.ejs", { fac });
 };
 
-exports.updateCoordinator = async function (req, res) {
-  console.log(req.body.id);
-  await FacCoordinator.findOneAndUpdate(
-    { _id: req.body.id },
-    {
-      coordImage: {
-        data: fs.readFileSync(
-          path.join(__dirname + "/../uploads/" + req.file.filename)
-        ),
-        contentType: "image/png",
-      },
-      sideText: req.body.sideText,
-      done: true,
-    }
-  );
-  await unlinkAsync(path.join(__dirname + "/../uploads/" + req.file.filename));
-  res.redirect("/coordinator/view-faccoord");
+exports.updateCoordinator = async function(req, res) {
+    // console.log(req.file);
+    await FacCoordinator.findOneAndUpdate({ _id: req.body.id }, {
+        coordImage: {
+            data: fs.readFileSync(
+                path.join(__dirname + "/../uploads/" + req.file.filename)
+            ),
+            contentType: "image/png",
+        },
+        sideText: req.body.sideText,
+        done: true,
+    });
+    await unlinkAsync(path.join(__dirname + "/../uploads/" + req.file.filename));
+    res.redirect("/coordinator/view-faccoord");
 };
 
-exports.addCoordinator = async function (req, res) {
-  const data = new FacCoordinator({
-    coordImage: {
-      data: fs.readFileSync(
-        path.join(__dirname + "/../uploads/" + req.file.filename)
-      ),
-      contentType: "image/png",
-    },
-    sideText: req.body.sideText,
-  });
-  await data.save();
-  await unlinkAsync(path.join(__dirname + "/../uploads/" + req.file.filename));
-  res.redirect("/coordinator/view-faccoord");
+exports.addCoordinator = async function(req, res) {
+    const data = new FacCoordinator({
+        coordImage: {
+            data: fs.readFileSync(
+                path.join(__dirname + "/../uploads/" + req.file.filename)
+            ),
+            contentType: "image/png",
+        },
+        sideText: req.body.sideText,
+    });
+    await data.save();
+    await unlinkAsync(path.join(__dirname + "/../uploads/" + req.file.filename));
+    res.redirect("/coordinator/view-faccoord");
 };
